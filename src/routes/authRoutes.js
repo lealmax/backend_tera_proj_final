a4a27c49d6ee972bdc0d6be091ff966362041642
lealmaxwell <maxwell.leal@hotmail.com>
2023-08-13 19:46:29 -0300

    Modifica authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,58 +1,67 @@
+// import express from "express";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import User from "../models/User.js";
+
+// const router = express.Router();
+
+// // Rota de autenticação de usuário
+// router.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(404).json({ message: "Usuário não encontrado." });
+//     }
+
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+//     if (!isPasswordValid) {
+//       return res.status(401).json({ message: "Credenciais inválidas." });
+//     }
+
+//     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+//       expiresIn: "1h",
+//     });
+
+//     res.json({ token });
+//   } catch (err) {
+//     console.error("Erro ao autenticar usuário:", err);
+//     res.status(500).json({ message: "Erro ao autenticar usuário." });
+//   }
+// });
+
+// // Rota de registro de novo usuário
+// router.post("/register", async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   try {
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return res.status(409).json({ message: "E-mail já registrado." });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const newUser = await User.create({
+//       name,
+//       email,
+//       password: hashedPassword,
+//     });
+
+//     res.status(201).json({ message: "Usuário registrado com sucesso." });
+//   } catch (err) {
+//     console.error("Erro ao registrar usuário:", err);
+//     res.status(500).json({ message: "Erro ao registrar usuário." });
+//   }
+// });
+
+// export default router;
+
 import express from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import authController from "../controllers/authContoller.js";
 
 const router = express.Router();
 
-// Rota de autenticação de usuário
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: "Usuário não encontrado." });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Credenciais inválidas." });
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-      expiresIn: "1h",
-    });
-
-    res.json({ token });
-  } catch (err) {
-    console.error("Erro ao autenticar usuário:", err);
-    res.status(500).json({ message: "Erro ao autenticar usuário." });
-  }
-});
-
-// Rota de registro de novo usuário
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(409).json({ message: "E-mail já registrado." });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    res.status(201).json({ message: "Usuário registrado com sucesso." });
-  } catch (err) {
-    console.error("Erro ao registrar usuário:", err);
-    res.status(500).json({ message: "Erro ao registrar usuário." });
-  }
-});
+router.post("/login", authController.authenticateUser);
 
 export default router;
